4a35beaf8f3b1d2d8160e7d84dc5718448d263ca
geethamrutha tummala <geethamrutha.tummala06@gmail.com>
2026-06-16 08:58:50 +0530

    Delete rag-document-qa project directory

--- rag-document-qa project/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Tests package for RAG Document Q&A System
-"""
-
-__all__ = ['test_document_processor', 'test_vector_store', 'test_qa_chain']
--- rag-document-qa project/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utils package for RAG Document Q&A System"""
-
-from .document_processor import DocumentProcessor
-from .vector_store import VectorStoreManager
-from .qa_chain import QAChainManager
-
-__all__ = ['DocumentProcessor', 'VectorStoreManager', 'QAChainManager']
